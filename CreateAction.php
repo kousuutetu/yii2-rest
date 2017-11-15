37006b70ab9a4a5f867970761581d12466f7715f
kousuutetu <kousuutetu@163.com>
2017-11-15 21:26:38 +0800

    recovery location to view

--- CreateAction.php
+++ CreateAction.php
@@ -2,6 +2,7 @@
 namespace ginkgo\rest;
 
 use Yii;
+use yii\helpers\Url;
 use yii\web\ServerErrorHttpException;
 
 class CreateAction extends \yii\rest\CreateAction
@@ -24,7 +25,10 @@ class CreateAction extends \yii\rest\CreateAction
 
         $model->load(Yii::$app->getRequest()->getBodyParams(), '');
         if ($model->save()) {
-            Yii::$app->getResponse()->setStatusCode(201);
+            $response = Yii::$app->getResponse();
+            $response->setStatusCode(201);
+            $id = implode(',', array_values($model->getPrimaryKey(true)));
+            $response->getHeaders()->set('Location', Url::toRoute([$this->viewAction, 'id' => $id], true));
         } elseif (!$model->hasErrors()) {
             throw new ServerErrorHttpException('Failed to create the object for unknown reason.');
         }
